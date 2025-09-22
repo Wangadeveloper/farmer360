e8b7988110ef1c465a5dd0f4925dc9eea38f668c
Engineer wanga <wanga.15065@students.kyu.ac.ke>
2025-09-22 06:33:10 +0300

    resolve leaked apis

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask,render_template
 from app.extensions import db, bcrypt, login_manager
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object("config.Config")
